0efd6af91462a934a5ce40356b96eaa01a3a9259
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2017-03-22 17:07:00 -0300

    page products

--- src/components/commom/Header.js
+++ src/components/commom/Header.js
@@ -4,8 +4,14 @@ import {Link, IndexLink} from 'react-router';
 const Header = () => {
   return(
     <nav>
+      <p>
+        meu filho
+      </p>
+
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
+      <Link to="/products" activeClassName="active">Products</Link>
+      {" |  "}
       <Link to="/about" activeClassName="active">About</Link>
     </nav>
   );
