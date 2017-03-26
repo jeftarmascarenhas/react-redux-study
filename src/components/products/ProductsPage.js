b26d9c706fbe80586071198b90c36b8e4a175bac
Jeftar Mascarenhas <jeftarmascarenhas@hotmail.com>
2017-03-26 17:39:45 -0300

    create basic flux of redux in app react-redux

--- src/components/products/ProductsPage.js
+++ src/components/products/ProductsPage.js
@@ -1,13 +1,58 @@
-import React, {Component, PropTypes} from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import * as productActions from '../../actions/productActions';
 
-class Products extends Component {
+class ProductsPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      product: { title: '' }
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange(event) {
+    const product = this.state.product;
+    product.title = event.target.value;
+    this.setState({product: product});
+  }
+
+  onClickSave(event) {
+    event.preventDefault();
+    this.props.dispatch(productActions.createProduct(this.state.product));
+  }
+
+  productRow(product, index) {
+    return <div key={index}>{product.title}</div>;
+  }
+
   render() {
     return(
       <div>
         <h1>Products</h1>
+        {this.props.products.map(this.productRow)}
+        <h2>Add Products</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          valeu={this.state.product.title} />
+        <input type="submit" value="Save" onClick={this.onClickSave}/>
       </div>
     );
   }
 }
 
-export default Products;
+function mapStateToProps(state, ownProps) {
+  return {
+    products: state.products
+  };
+}
+
+ProductsPage.propTypes = {
+  dispatch: React.PropTypes.func.isRequired,
+  products: React.PropTypes.array.isRequired
+};
+
+export default connect(mapStateToProps)(ProductsPage);
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import HomePage from './components/home/HomePage';
 import AboutPage from './components/about/AboutPage';
-import Products from './components/products/ProductsPage';
+import ProductsPage from './components/products/ProductsPage';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage} />
     <Route path="about" component={AboutPage} />
-    <Route path="products" component={Products} />
+    <Route path="products" component={ProductsPage} />
   </Route>
 );
